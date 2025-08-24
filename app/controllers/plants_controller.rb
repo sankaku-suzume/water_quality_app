d498d4529a7b6bc012a31b1a5298c3511fd2a6cd
さんかく <sankaku.suzume@gmail.com>
2025-08-24 16:58:03 +0900

    new、createアクション

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -6,4 +6,23 @@ class PlantsController < ApplicationController
   def show
     @plant = Plant.find(params[:id])
   end
+
+  def new
+    @plant = Plant.build
+  end
+
+  def create
+    @plant = Plant.build(plant_params)
+    if @plant.save
+      redirect_to plant_path(@plant), notice: '保存しました'
+    else
+      flash.now[:error] = '保存に失敗しました'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def plant_params
+    params.require(:plant).permit(:name, :location, :remarks)
+  end
 end
