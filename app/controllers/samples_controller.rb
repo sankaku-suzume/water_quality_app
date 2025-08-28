41aec76b3bb5abe7d2a3796b09a283420aba06b3
さんかく <sankaku.suzume@gmail.com>
2025-08-28 22:05:19 +0900

    Sample#new、#create

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -1,12 +1,47 @@
 class SamplesController < ApplicationController
+  before_action :set_plant, only: [:show, :new, :create, :edit, :update, :destroy]
 
   def index
     @samples = Sample.all.order(sampling_date: :desc)
   end
 
   def show
-    plant = Plant.find(params[:plant_id])
-    @sample = plant.samples.find(params[:id])
+    @sample = @plant.samples.find(params[:id])
+  end
+
+  def new
+    @sample = @plant.samples.build
+  end
+
+  def create
+    @sample = @plant.samples.build(sample_params)
+    if @sample.save
+      redirect_to plant_sample_path(@plant, @sample), notice: '保存しました'
+    else
+      flash.now[:error] = '保存に失敗しました'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+  def sample_params
+    params.require(:sample).permit(:sampling_date, :sampling_time, :location, :inspector, :remarks )
+  end
+
+  def set_plant
+    @plant = Plant.find(params[:plant_id])
   end
 
 end
