ba3ac20ef8bd391c1daf6aa1c5a07e16bbdb4bcd
さんかく <sankaku.suzume@gmail.com>
2025-09-01 05:55:14 +0900

    TestItem#new,create

--- app/controllers/test_items_controller.rb
+++ app/controllers/test_items_controller.rb
@@ -4,4 +4,23 @@ class TestItemsController < ApplicationController
     @test_items = TestItem.all.order('sort_order')
   end
 
+  def new
+    @test_item = TestItem.build
+  end
+
+  def create
+    @test_item = TestItem.build(test_item_params)
+    if @test_item.save
+      redirect_to test_items_path, notice: '保存しました'
+    else
+      flash.now[:error] = '保存に失敗しました'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def test_item_params
+    params.require(:test_item).permit(:name, :unit, :detection_limit, :standard_max, :standard_min, :sort_order)
+  end
+
 end
