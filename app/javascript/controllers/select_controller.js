150ed8f0b174b587b3e1108adc5f4d18f8297e42
さんかく <sankaku.suzume@gmail.com>
2025-09-17 05:46:47 +0900

    formで検査項目を選択すると単位をリアルタイムで表示させる

--- app/javascript/controllers/select_controller.js
+++ app/javascript/controllers/select_controller.js
@@ -3,12 +3,17 @@ import { Controller } from "@hotwired/stimulus"
 export default class SelectController extends Controller {
   static targets = ['testItem']
 
-  connect() {
-    console.log('SelectController connected')
-  }
+  // connect() {
+  //   console.log('SelectController connected')
+  // }
 
   change(event) {
-    console.log("changeイベントが呼ばれた！")
-
+    const elements = document.querySelectorAll('.result-form_unit')
+    elements.forEach(element => {
+      element.classList.add('hidden')
+    })
+    const testItemId = event.target.value
+    const unit = document.getElementById(`result-form_unit-test_item_id${testItemId}`)
+    unit.classList.remove('hidden')
   }
 }
