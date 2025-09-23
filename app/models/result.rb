009c503425d07e461dc019dc1eb46bd7c9411a79
さんかく <sankaku.suzume@gmail.com>
2025-09-23 13:54:20 +0900

    過去20件の検査結果の平均・最小・最大値を表示

--- app/models/result.rb
+++ app/models/result.rb
@@ -19,4 +19,30 @@ class Result < ApplicationRecord
   belongs_to :test_item
 
   validates :value, numericality: { allow_nil: true }
+
+  def average(results)
+    values = set_values(results)
+    values.sum.fdiv(results.length)
+  end
+
+  def minimum(results)
+    values = set_values(results)
+    values.min
+  end
+
+  def maximum(results)
+    values = set_values(results)
+    values.max
+  end
+
+  private
+  def set_values(results)
+    values = []
+    results.each do |result|
+      value = result.value
+      values.push(value)
+    end
+    values
+  end
+
 end
