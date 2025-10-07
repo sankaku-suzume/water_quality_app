b57b678aee50e4a7959d48287d1582eada85c35a
さんかく <sankaku.suzume@gmail.com>
2025-10-08 05:53:05 +0900

    検索をインクリメンタルサーチ化

---
+++ app/javascript/controllers/search_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="search"
+export default class extends Controller {
+  submit() {
+    clearTimeout(this.timeout)
+    
+    this.timeout = setTimeout(() => {
+      this.element.requestSubmit()
+
+    }, 200)
+  }
+}
