3e734664cc76d78721a4e16590786e8677514c6a
さんかく <sankaku.suzume@gmail.com>
2025-12-21 11:57:11 +0900

    resultのformでeditを開いたときに単位を表示するように修正

--- app/javascript/controllers/select_controller.js
+++ app/javascript/controllers/select_controller.js
@@ -3,9 +3,11 @@ import { Controller } from "@hotwired/stimulus"
 export default class SelectController extends Controller {
   static targets = ['testItem']
 
-  // connect() {
-  //   console.log('SelectController connected')
-  // }
+  connect() {
+    console.log('SelectController connected')
+    const testItemId = this.testItemTarget.value
+    this.removeHidden(testItemId)
+   }
 
   change(event) {
     const elements = document.querySelectorAll('.result-form_unit')
@@ -13,6 +15,10 @@ export default class SelectController extends Controller {
       element.classList.add('hidden')
     })
     const testItemId = event.target.value
+    this.removeHidden(testItemId)
+  }
+
+  removeHidden(testItemId) {
     const unit = document.getElementById(`result-form_unit-test_item_id${testItemId}`)
     unit.classList.remove('hidden')
   }
