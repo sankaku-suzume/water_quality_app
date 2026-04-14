ecc6420599659a2077eb2e5f1af3514ae9b73327
さんかく <sankaku.suzume@gmail.com>
2026-04-14 22:33:05 +0900

    1つ前のapprovalの値でバリデーションを設定

--- app/models/approval.rb
+++ app/models/approval.rb
@@ -20,6 +20,7 @@ class Approval < ApplicationRecord
   validates :comment, length: { maximum: 100 }
 
   validate :check_creatable
+  validate :check_previous_action
 
   enum action: {
     requested: 0,
@@ -31,6 +32,18 @@ class Approval < ApplicationRecord
     def check_creatable
       if result.approved?
         errors.add(:base, '承認済みです')
+      end
+    end
+
+    def check_previous_action
+      previous_approval = result.approvals.where.not(id: self.id).order(created_at: :desc).first
+
+      if previous_approval.nil? && !self.requested?
+        errors.add(:base, '承認依頼してください')
+      elsif previous_approval&.requested? && self.requested?
+        errors.add(:base, 'すでに承認依頼中です')
+      elsif previous_approval&.rejected? && self.rejected?
+        errors.add(:base, 'すでに差戻中です')
       end
     end
 end
